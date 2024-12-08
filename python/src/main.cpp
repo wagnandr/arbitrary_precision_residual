69b19e8933d9331f469eb4884bfcd9c90ceb5a01
wagnandr <wagnandr@in.tum.de>
2024-12-08 19:38:20 +0100

    slowly trying to debug my pybind11 modules

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -1,36 +1,14 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 #include "highprecision_csr.hpp"
 
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-
 namespace py = pybind11;
-namespace mc = macrocirculation;
-
-static int len = 0;
-static char * program_name = "";
-static char ** argv = &program_name;
 
 PYBIND11_MODULE(_core, m) {
-  m.doc() = R"pbdoc(
-        HPResidual library
-        -----------------------
-        .. currentmodule:: hpresidual 
-        .. autosummary::
-           :toctree: _generate
-    )pbdoc";
-
+  m.doc() = "pybind11 example plugin"; // optional module docstring
   m.def("set_precision", set_precision , "Sets the default precision in our library");
+  m.def("test", [](){}, "Sets the default precision in our library");
 
   py::class_<CSRMatrix>(m, "CSRMatrix")
     ;
 
-
-
-#ifdef VERSION_INFO
-  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-  m.attr("__version__") = "dev";
-#endif
 }
